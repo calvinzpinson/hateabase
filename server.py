5eb3143b5d2adec6bf1c0cf576d47ed1138adeb7
marinamoore <mmoore32@calpoly.edu>
2016-12-06 22:23:43 -0800

    added a drop down

--- server.py
+++ server.py
@@ -5,12 +5,13 @@ try:
     from mysql.connector import connect
     from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 except:
-    try:
-        pip.main(['install','-r','requirements.pip'])
-        from mysql.connector import connect
-        from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-    except:
-        die(-1)
+    print("Missing requirements\n")
+    #try:
+        #pip.main(['install','-r','requirements.pip'])
+        #from mysql.connector import connect
+        #from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+    #except:
+    die(-1)
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -18,6 +19,14 @@ app.config.from_object(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/hateabase/', methods=['POST', 'GET'])
+def hateabase():
+    if request.method == 'POST':
+        result = request.form['Category']
+    else:
+        result = ""
+    return render_template('base.html', result=result)
 
 def main():
     port_num = 5000
