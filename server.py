f18b6a75c23e62fed0c475b2e37092182e711ddf
Calvin <calvinzpinson@gmail.com>
2016-12-07 20:29:01 -0800

    Test method for db stuff, database initialization

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def home():
+    test()
     return render_template('index.html')
 
 @app.route('/hateabase/', methods=['POST', 'GET'])
@@ -23,24 +24,18 @@ def hateabase():
         result = ""
     return render_template('base.html', result=result)
 
-def reset():
-    db = get_db()
-    
-
 def get_db():
     if not hasattr(g, "mysql_db"):
         g.mysql_db = connect()
     return g.mysql_db
         
 def connect():
-    configParser = configparser.ConfigParser()
-    configFilePath = "./dbconf.365"
-    configParser.read(configFilePath)
+    configData = getConfigData()
 
-    host = configParser.get("CSLVM",'host')
-    user = configParser.get("CSLVM",'userid')
-    password = configParser.get("CSLVM",'password')
-    database = configParser.get("CSLVM", 'database')
+    host = configData.get("DigitalOcean", 'host')
+    user = configData.get("DigitalOcean", 'userid')
+    password = configData.get("DigitalOcean", 'password')
+    database = configData.get("DigitalOcean", 'database')
 
     try:
         conn = mysql.connector.connect(
@@ -54,6 +49,48 @@ def connect():
 
     return conn
 
+def getConfigData():
+    configParser = configparser.ConfigParser()
+    configFilePath = "./dbconf.365"
+    configParser.read(configFilePath)
+
+    return configParser
+
+def reInitializeDatabase():
+    
+    db = get_db()
+    configData = getConfigData()
+    queryFiles = [configData.get("SQL", "destroy"), 
+                  configData.get("SQL", "create"), 
+                  configData.get("SQL", "insert")]
+
+    for queryFile in queryFiles:
+        executeSqlFromFile(queryFile, db)
+
+def executeSqlFromFile(file, db):
+    try:
+        with open("./queries/" + file, 'r') as fd:
+            sqlFile = fd.read()
+            queries = sqlFile.split(";")
+            cursor = db.cursor()
+            for query in queries:
+                try:
+                    cursor.execute(query)
+                except OperationalError:
+                    print("Failed to execute sql from file: ")
+
+    except IOError:
+        print("Failed to open sql file")
+        exit(-1)
+    finally:
+        cursor.close()
+
+def test():
+    SQL = "CREATE TABLE HelloWorld (hello VARCHAR(10), world VARCHAR(10));"
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute(SQL)
+
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, "mysql_db"):
@@ -63,7 +100,6 @@ def main():
     port_num = 5000
     if len(argv) == 2:
         port_num = int(argv[1])
-    connect()
     app.run(port=port_num)
 
 if __name__ == '__main__':
